ea4383ab69c0554d4df2816d99e63d1bbe8ff818
ModistAndrew <zhujinxuan2005@163.com>
2024-05-31 11:25:45 +0800

    TLE

--- src/TicketSystem.cpp
+++ src/TicketSystem.cpp
@@ -17,6 +17,9 @@ void checkCache() {
 int main() {
 //  freopen("testcases/pressure_1_easy/44.in", "r", stdin);
 //  freopen("test.out", "w", stdout);
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+  std::cout.tie(nullptr);
   Commands::init();
   while (Commands::running) {
     std::string input;
