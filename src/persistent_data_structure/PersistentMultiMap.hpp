bc428922daae5917c0bb190b01b8cdf0fd4aab3e
ModistAndrew <zhujinxuan2005@163.com>
2024-06-01 16:52:48 +0800

    minimize memory usage

--- src/persistent_data_structure/PersistentMultiMap.hpp
+++ src/persistent_data_structure/PersistentMultiMap.hpp
@@ -31,8 +31,8 @@ class PersistentMultiMap {
     }
   };
 
-  static constexpr int SIZE_1 = 2048 / sizeof(T) * 2;
-  static constexpr int SIZE_2 = 2048 / sizeof(INDEX) * 2;
+  static constexpr int SIZE_1 = 1000 / sizeof(T) * 2;
+  static constexpr int SIZE_2 = 1000 / sizeof(INDEX) * 2;
 
   static_assert(SIZE_1 >= 4 && SIZE_1 % 2 == 0, "SIZE_1 must be even and at least 4");
   static_assert(SIZE_2 >= 4 && SIZE_2 % 2 == 0, "SIZE_2 must be even and at least 4");
